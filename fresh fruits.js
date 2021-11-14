4e20fe24dffb3c6a0992b1a84435a4b9a60e8e37
Arundhathi R Menon <43034600+arundhathi6@users.noreply.github.com>
2021-11-14 08:15:00 +0530

    Add files via upload

---
+++ fresh fruits.js
@@ -0,0 +1,445 @@
+var fruits_list = [{
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "25",
+    food_type: "veg",
+    product_MRP: "125.00",
+    product_name: "Apple Indian 6 pcs (Approx 750 g - 950 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590004487/apple-indian-6-pcs-pack-0-20200806.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "20",
+    food_type: "veg",
+    product_MRP: "49.00",
+    product_name: "Mosambi 6 pcs (Approx 750 g - 950 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590000448/mosambi-1-kg-0-20201118.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "25",
+    food_type: "veg",
+    product_MRP: "44.00",
+    product_name: "Musk Melon 3 pcs (Approx 750 g - 950 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590000051/musk-melon-1-kg-0-20201118.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "10",
+    food_type: "veg",
+    product_MRP: "169.00",
+    product_name: "Dates Imported (Approx 400 g - 500 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590002744/dates-imported-400-g-pack-0-20201118.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "0",
+    food_type: "veg",
+    product_MRP: "36.00",
+    product_name: "Banana Robusta 6 Pcs (Box) (Approx 800 g - 1100 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590001285/banana-robusta-6-pcs-box-0-20201126.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "30",
+    food_type: "veg",
+    product_MRP: "54.00",
+    product_name: "Watermelon Kiran 1 pc (Approx 2300 g - 3000 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590041413/watermelon-kiran-1-pc-0-20201009.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "5",
+    food_type: "veg",
+    product_MRP: "179.00",
+    product_name: "Orange Imported 6 pcs (Approx 750 g - 950 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590000025/orange-imported-1-kg-0-20201118.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "0",
+    food_type: "veg",
+    product_MRP: "45.00",
+    product_name: "Orange 6 pcs (Pack) (Approx 750 g - 900 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590034369/orange-6-pcs-pack-0-20200917.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "15",
+    food_type: "veg",
+    product_MRP: "219.00",
+    product_name: "Pomegranate Kesar 4 pcs (Approx 800 g- 1000 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590144486/pomegranate-kesar-4-pcs-approx-800-g-1000-g-0-20210304.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "16",
+    food_type: "veg",
+    product_MRP: "59.00",
+    product_name: "Pineapple Queen 1 pc (Approx 700 g - 1200 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590000070/pineapple-queen-1-pc-0-20201119.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "50",
+    food_type: "veg",
+    product_MRP: "119.00",
+    product_name: "Kiwi Green Zespri 3 pcs (Pack) (Approx 240 g - 300 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590034325/kiwi-green-zespri-3-pcs-pack-0-20200911.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "15",
+    food_type: "veg",
+    product_MRP: "179.00",
+    product_name: "Baby Kiwi Imported 6 Pcs (Approx 400 g - 550 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590008484/baby-kiwi-6-pcs-0-20201118.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "10",
+    food_type: "veg",
+    product_MRP: "44.00",
+    product_name: "Papaya (Each) (Approx 800 g - 1600 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590001247/papaya-each-0-20201118.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "0",
+    food_type: "veg",
+    product_MRP: "89.00",
+    product_name: "Banana Yellaki 12 pcs (Approx 750 g - 1000 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590041069/banana-yellaki-12-pcs-0-20200928.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "10",
+    food_type: "veg",
+    product_MRP: "169.00",
+    product_name: "Pomegranate Economy 6 pcs (Pack)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590485022/pomegranate-economy-6-pcs-pack-0-20210816.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "30",
+    food_type: "veg",
+    product_MRP: "150.00",
+    product_name: "Apple Red Delicious 4 pcs (Approx 550 g - 700 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590001721/apple-red-delicious-4-pcs-pack-0-20200806.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "20",
+    food_type: "veg",
+    product_MRP: "139.00",
+    product_name: "Apple Royal Gala (4 pcs) (Approx 500 g - 700 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590001723/apple-royal-gala-4-pcs-0-20201118.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "0",
+    food_type: "veg",
+    product_MRP: "99.00",
+    product_name: "Sweet Tamarind (Pack) (Approx 240 g - 260 ",
+    product_url: "https://www.jiomart.com/images/product/150x150/590000695/sweet-tamarind-250-g-0-20200731.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "25",
+    food_type: "veg",
+    product_MRP: "49.00",
+    product_name: "Tender Coconut Cling Wrapped (1 pc) (Approx 900 g - 1300 g)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590003564/tender-coconut-per-piece-0-20201118.jpg",
+  },
+  {
+    product_brand: "Private Label",
+    product_information: {
+        seller: "sold by Reliance Retail",
+        country_of_origin: "India",
+    },
+    disclaimer: "Despite our attempts to provide you with the most accurate information possible, the actual packaging, ingredients and colour of the product may sometimes vary. Please read the label, directions and warnings carefully before use.",
+    product_discount: "40",
+    food_type: "veg",
+    product_MRP: "124.00",
+    product_name: "Pears Green Imported 3 pcs (Pack)",
+    product_url: "https://www.jiomart.com/images/product/150x150/590040932/pears-imported-green-3-pcs-pack-0-20201031.jpg",
+  },
+  ];
+  
+  
+  
+  
+  //Don't touch anything inside this block
+  localStorage.setItem("fruitsList", JSON.stringify(fruits_list));
+  var jiomart = JSON.parse(localStorage.getItem("fruitsList"));
+  var listoffruits = document.getElementById("listoffruits");
+  
+  displayFruits();
+  
+  var count = 0;
+  
+  function displayFruits() {
+  listoffruits.innerHTML = "";
+  count = 0;
+  jiomart.map(function(el, index) {
+    var imageDiv = document.createElement("div");
+    imageDiv.setAttribute("id", "imageDiv" + count);
+    imageDiv.style.transition = "all 2s";
+    
+    var img = document.createElement("img");
+    img.setAttribute("id","image");
+    img.setAttribute("src", el.product_url);
+   
+    var h4tag = document.createElement("h4");
+    h4tag.textContent = el.product_name;
+    h4tag.style.fontWeight = "lighter";
+    var h3tag = document.createElement("h3");
+    h3tag.style.fontWeight = "lighter";
+    h3tag.textContent = "₹ " + Number(el.product_MRP);
+  
+    var button = document.createElement("button");
+    button.textContent = "Add to Cart";
+    button.setAttribute("class", "cartId");
+    imageDiv.append(img, h4tag, h3tag, button);
+    listoffruits.append(imageDiv);
+    button.addEventListener("click", function(e) {
+        addQty(e, index, el);
+        addCart(el);
+    });
+    count++;
+  });
+  }
+  
+  //adding functionality to add to cart button
+  var cart = JSON.parse(localStorage.getItem("MyCart")) || [];
+  var stack = [];
+  var click = 0;
+  var total_quantity = 1;
+  
+  function addQty(e, index, el) {
+  total_quantity = 1;
+  var quantity = 1;
+  e.target.remove();
+  var btnDiv = document.createElement("div");
+  btnDiv.setAttribute("class", "btnDiv");
+  var btn1 = document.createElement("button");
+  btn1.textContent = "+";
+  var h3tag = document.createElement("h3");
+  h3tag.setAttribute("class", "h3tag");
+  h3tag.textContent = quantity;
+  var btn2 = document.createElement("button");
+  btn2.textContent = "-";
+  btnDiv.append(btn2, h3tag, btn1);
+  document.getElementById("imageDiv" + index).append(btnDiv);
+  
+  btn1.addEventListener("click", function(e) {
+    quantity++;
+    total_quantity++;
+    h3tag.textContent = quantity;
+    total_quantity = quantity;
+    var textContent = h3tag.textContent;
+    addCart(el, textContent);
+  });
+  btn2.addEventListener("click", function() {
+    if (quantity >= 1) {
+        quantity--;
+        h3tag.textContent = quantity;
+        total_quantity--;
+        var textContent = h3tag.textContent;
+        addCart(el, textContent);
+    } else if (quantity == 0) {
+        btnDiv.remove();
+        var button = document.createElement("button");
+        button.textContent = "Add to Cart";
+        button.setAttribute("class", "cartId");
+        document.getElementById("imageDiv" + index).append(button);
+    }
+  });
+  click++;
+  }
+  
+  function addCart(el, textContent) {
+  if (textContent == undefined) {
+    el.total_quantity = 1;
+    stack.push(el);
+  } else {
+    el.total_quantity = textContent;
+    stack.push(el);
+  }
+  }
+  document.querySelector("#clickme").addEventListener("click", toChart);
+  
+  function toChart() {
+  window.location.href = "mycart.html";
+  
+  cart = stack.filter((c, index) => {
+    return stack.indexOf(c) === index;
+  });
+  
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].total_quantity == "0") {
+        cart.splice(i, 1);
+    }
+  }
+  localStorage.setItem("MyCart", JSON.stringify(cart));
+  window.location.href = "mycart.html";
+  }
+  //adding functionality to add to cart button ends here
+  
+  
+  
+  // sorting functionality function start here 
+  
+  document.getElementById("low").addEventListener("click", function() {
+  
+  handleSort("low");
+  });
+  document.getElementById("high").addEventListener("click", function()  {
+  handleSort("high");
+  });
+  document.getElementById("discount").addEventListener("click", () => {
+  handleSort("discount")
+  });
+  document.getElementById("all").addEventListener("click", () => {
+  handleSort("all");
+  });
+  
+  
+  
+  
+  
+  function handleSort(selected) {
+  jiomart = JSON.parse(localStorage.getItem("fruitsList"));
+  if (selected == "low") {
+  
+    jiomart = jiomart.sort(function(a, b) {
+  
+        return a.product_MRP - b.product_MRP;
+    })
+  }
+  
+  if (selected == "high") {
+    jiomart = jiomart.sort(function(a, b) {
+        return b.product_MRP - a.product_MRP;
+    })
+  }
+  
+  if (selected == "discount") {
+    jiomart = jiomart.sort(function(a, b)  {
+        return a.product_discount - b.product_discount;
+    })
+  }
+  
+  if (selected == "all") {
+    jiomart = JSON.parse(localStorage.getItem("fruitsList"));
+  }
+  
+  displayFruits(jiomart);
+  }
+  
+  
+  //sorting button active effect
+  
+  var header = document.getElementById("sort-option");
+  var btns = header.getElementsByClassName("btn");
+  for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+  });
+  }
